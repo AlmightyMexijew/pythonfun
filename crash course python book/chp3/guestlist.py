50009f9a3eeb996fe318afcbe1158bfd75f0eb6c
Gershon <CTepper625@gmail.com>
2022-04-19 14:34:17 +0300

    new files

---
+++ crash course python book/chp3/guestlist.py
@@ -0,0 +1,9 @@
+guest_list = ['odelia', 'david b', 'iddo', 'juju', 'safta cat']
+message = f"Hi {guest_list[0]} , I want to invite you to dinner!"
+print(message)
+message = f"Hi {guest_list[1]}, I want to invite you to dinner!"
+print(message)
+message = f"Hi {guest_list[2]} , I want to invite you to dinner!"
+print(message)
+message = f"Hi {guest_list[3]} , I want to invite you to dinner!"
+print(message)
---
+++ crash course python book/chp3/guestlist2.py
@@ -0,0 +1,21 @@
+guest_list = ['odelia', 'david b', 'iddo', 'juju', 'safta cat']
+message = f"Hi {guest_list[0]} , I want to invite you to dinner!"
+print(message)
+message = f"Hi {guest_list[1]}, I want to invite you to dinner!"
+print(message)
+message = f"Hi {guest_list[2]} , I want to invite you to dinner!"
+print(message)
+message = f"Hi {guest_list[3]} , I want to invite you to dinner!"
+print(message)
+cantmake = guest_list[3]
+print(cantmake + " we're sorry to hear you cannot make it")
+guest_list.remove('iddo')
+guest_list.append('max')
+print(message)
+message = f"Hi {guestlist[0]}, I want to invite you to dinner!"
+print(message)
+message = f"Hi {guest_list[1]}, I want to invite you to dinner!"
+print(message)
+message = f"Hi {guest_list[2]}, I want to invite you to dinner!"
+print(message)
+message = f"Hi {guest_list[3]}, I want to invite you to dinner!"
