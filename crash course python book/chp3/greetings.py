1c22bc989076ca19b3f6461a74bab92cd3824df9
Gershon <CTepper625@gmail.com>
2022-04-19 10:40:51 +0300

    new day, new chp3 stuff

---
+++ crash course python book/chp3/greetings.py
@@ -0,0 +1,11 @@
+names = ['David','Bob','Mike','Jerry','Odelia']
+message = f"Hi {names[0].title()}. I am Gershon. I am the best"
+print(message)
+message = f"Hi {names[1].title()}. I am Gershon. I am the best"
+print(message)
+message = f"Hi {names[2].title()}. I am Gershon. I am the best"
+print(message)
+message = f"Hi {names[3].title()}. I am Gershon. I am the best"
+print(message)
+message = f"Hi {names[4].title()}. I am Gershon. I am the best"
+print(message)
---
+++ crash course python book/chp3/mylist.py
@@ -0,0 +1,10 @@
+cars = ['Toyota','Ford','Honda','Hyundai','Pontiac']
+message = f"I would like my old {cars[0]} Corolla back."
+print(message)
+message = f"I would never buy a {cars[1]}. They suck. He was a nazi."
+print(message)
+message = f"I once wanted a {cars[2]} Civic. It never came to be."
+print(message)
+message = f"Did you know that {cars[3]} is Korean?"
+print(message)
+message = f"I would sell my soul for rock n roll...or a {cars[4]}. I should've had a firebird."
