db27c039fd266c5825b0c0b35f3d515253f64d46
Gershon <CTepper625@gmail.com>
2022-04-19 12:12:24 +0300

    new files

---
+++ crash course python book/chp3/motoremove.py
@@ -0,0 +1,12 @@
+motorcycles = ['honda','yamaha','suzuki','ducati']
+print(motorcycles)
+
+motorcycles.remove('ducati')
+print(motorcycles)
+
+motorcycles.append('ducati')
+print(motorcycles)
+
+motorcycles.remove('yamaha')
+print(motorcycles)
+print("If this worked, yamaha will be gone")
---
+++ crash course python book/chp3/motoremove2.py
@@ -0,0 +1,14 @@
+motorcycles = ['honda','yamaha','suzuki','ducati']
+print(motorcycles)
+
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print(f"\nA {too_expensive.title()} is too expensive for me.")
+
+#line 1 assigns our list
+#line 2 prints
+#line 4 assigns variable too_expensive with value 'ducati'
+#line 5 removes whatever is in variable "too_expensive".
+#line 6 prints.
+#line 7 prints a formatted sentence
